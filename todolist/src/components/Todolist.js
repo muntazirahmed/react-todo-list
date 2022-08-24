a808454aef19ae89f5ec77b42a115c0de9d1d78f
muntazirahmed <muntazirahmed313@gmail.com>
2022-08-24 11:33:56 +0530

    usestate with inputfield

--- todolist/src/components/Todolist.js
+++ todolist/src/components/Todolist.js
@@ -1,16 +1,24 @@
 import React,{useState} from 'react';
 import Tododate from './Tododate';
-
+ 
 function Todolist(props) {
-    const[state,setstate]=useState('')
+    const[state,setstate]=useState('');
+    const[title,settitle]=useState('props.title')
+
+    const clickhandler =()=>{ 
+        settitle(state)
+    }
+    const changehandler =(event)=>{ 
+        setstate(event.target.value)
+    }
 
   return (
     <div>
 
         <Tododate tododate={props.date}/>
-        <div>{props.name}</div><br></br>
-        <div><input type="text"></input></div>
-        <div><button>Add to do</button></div>
+        <div>{title}</div><br></br>
+        <div><input onChange={changehandler} type="text" ></input></div>
+        <div><button onClick={clickhandler} >Add to do</button></div>
         
         
 
